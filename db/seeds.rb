4d5088633ec2b6a962e9641f0e7f0f7e6459762b
Cecile Gressard <cecile.gressard@edhec.com>
2018-10-17 16:08:11 +0200

    rake parfait

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant1 = Restaurant.new(
+  name: "Restaurant 1",
+  address: "1, avenue Victor Hugo",
+  phone_number: "0100000000",
+  category: "chinese"
+)
+restaurant1.save!
+
+restaurant1 = Restaurant.new(
+  name: "Restaurant 2",
+  address: "2, avenue Victor Hugo",
+  phone_number: "0200000000",
+  category: "italian"
+)
+restaurant1.save!
+
+restaurant1 = Restaurant.new(
+  name: "Restaurant 3",
+  address: "3, avenue Victor Hugo",
+  phone_number: "0300000000",
+  category: "japanese"
+)
+restaurant1.save!
+
+restaurant1 = Restaurant.new(
+  name: "Restaurant 4",
+  address: "4, avenue Victor Hugo",
+  phone_number: "0400000000",
+  category: "french"
+)
+restaurant1.save!
+
+restaurant1 = Restaurant.new(
+  name: "Restaurant 5",
+  address: "5, avenue Victor Hugo",
+  phone_number: "0500000000",
+  category: "belgian"
+)
+restaurant1.save!
